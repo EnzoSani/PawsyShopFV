3c806f68bde1064bcfc02456fe1db197c73983be
Enzo <enzosanabria23@gmail.com>
2025-10-23 21:09:09 -0300

    Add Nuget Packages for Entity Framework

---
+++ Pawsy.Domain/Entities/Pet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pawsy.Domain.Entities
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public int Age { get; private set; }
+        public string? Gender { get; private set; }
+        public string? ImageUrl { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public int CategoryId { get; private set; }
+        public Category Category { get; private set; } = null!;
+    }
+}
