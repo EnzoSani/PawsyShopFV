b7e41d1f044e302226b779477eb3913aa5c662a0
Enzo <enzosanabria23@gmail.com>
2025-10-29 21:12:16 -0300

    Add CategoryPetProductDtos

---
+++ Pawsy.Application/Dtos/ProductDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pawsy.Application.Dtos
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public double Price { get; set; }
+        public int Stock { get; set; }
+        public string? Brand { get; set; }
+        public bool IsFeatured { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
+        public int CategoryId { get; set; }
+        public int PetId { get; set; }
+    }
+}
